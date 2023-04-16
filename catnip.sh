8bcfd3d0fc96ee074530b203acf419c1977b8b6c
sweetbbak <sweetbabyalaska@gmail.com>
2023-04-15 22:03:47 -0800

    nya nya nyaaa

--- catnip.sh
+++ catnip.sh
@@ -8,6 +8,12 @@ i=0
 size=40
 lx=0
 ly=1
+array_count="${#array[@]}"
+count="(${i}/${array_count})"
+
+bg="\e[1;3;38;5;50;48;5;212m"
+clr="\e[0m"
+
 #save screen
 tput smcup
 
@@ -22,9 +28,25 @@ cleanup() {
 }
 
 show_image() {
+    # tput cup 0 0
+    # tput cup $LINES 0;
+        # tput el
+        # tput cud1
+
+    tput cup 0 0
+    tput el
+    # printf "\e[1;35H\e[40m\e[2K%s\e[0m\e[2;0H" "${count} ${image}"
+    printf "\e[40m\e[2K%s\e[0m\e[2;0H" "${count} ${image}"
+    tput cup 1 0
+    
     [ -f "$image" ] && { 
         kitty +kitten icat --clear --scale-up --place "${size}x${size}@${lx}x${ly}" "${image}"
-        printf "\e[1;35H\e[40m\e[2K%s\e[0m\e[2;0H" "$image"
+        # printf "\e[1;35H\e[40m\e[2K%s %s\e[0m\e[2;0H" "$count" "$image"
+        # tput cup $LINES 0;
+        # gum style --align=center --width=$COLUMNS \
+        #     --background=212 --foreground=50 \
+        #     --bold --italic "${count} ${image}"
+        # printf "%s%s%s" "${bg}" "$image" "$clr"
     }
     tput cup 1 0
 }
@@ -34,10 +56,25 @@ trap cleanup EXIT INT
 # Hide the cursor (there is probably a much better way to do this)
 printf '\e[?25l'
 
+keyhandler() {
+    case "$1" in
+    j) ;;
+    k);;
+
+    esac
+}
+
 while true; do
     read -r -s -n 1 key < /dev/stdin > /dev/null
     # clear below x line with tput
-    tput cup 1 0
+    # tput cup 1 0
+    array_count="${#array[@]}"
+    count="(${i}/${array_count})"
+    # check if count is less than 0 ie negative signed
+    # and subtract from total to get the correct index
+    if [[ "$i" -lt 0 ]]; then
+        array_count=$((array_count + i))
+    fi
 
     if [[ "$key" == j ]]; then
         i=$((i+1))
@@ -90,11 +127,15 @@ while true; do
         cb copy "$image"
     fi
 
-    # if [[ "$key" == o ]]; then
-    #     image="${array[$i]}"
-    #     image="${image// /\ }"
-    #     krita "$image"
-    # fi
+    if [[ "$key" == o ]]; then
+        image="${array[$i]}"
+        image="${image// /\ }"
+        nsxiv "$image"
+    fi
+
+    if [[ "$key" == c ]]; then
+        clear
+    fi
 
     if [[ "$key" == q ]]; then
         break
