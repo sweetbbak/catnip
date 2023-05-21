ece9fa90bd73285fd142de95c95d77ff82fd4578
sweetbbak <sweetbabyalaska@gmail.com>
2023-05-21 13:40:56 -0800

    add python version

---
+++ catnip3.py
@@ -0,0 +1,136 @@
+#!/bin/env python3
+# from rich import print
+from rich.panel import Panel
+from rich.console import Console
+from rich.layout import Layout
+from rich.table import Table
+
+# from time import sleep
+from rich.live import Live
+import os
+import blessed
+
+term = blessed.Terminal()
+# from rich import box
+# from rich.align import Align
+
+
+console = Console()
+home = os.getenv("HOME")
+pics = os.path.join(home, "Pictures")
+cwd = os.getcwd()
+
+
+def walkimg(dir):
+    images = []
+    for root, subdirs, files in os.walk(dir):
+        for filename in files:
+            img = os.path.join(root, filename)
+            if img.endswith("png") and os.path.exists(img):
+                images.append(img)
+            elif img.endswith("jpg") and os.path.exists(img):
+                images.append(img)
+            elif img.endswith("gif") and os.path.exists(img):
+                images.append(img)
+    return images
+
+
+# image path and dimensions
+def refresh(image, x, y, w):
+    os.system(f'kitty +kitten icat --clear --scale-up --place {w}x{w}@{x}x{y} "{image}"')
+
+
+def make_layout() -> Layout:
+    layout = Layout(name="Pictures")
+
+    layout.split(
+        Layout(name="Title", size=3),
+        Layout(name="main"),
+        Layout(name="command", size=3),
+        Layout(name="command2", size=3),
+    )
+    return layout
+
+
+class Header:
+    """Display Image Title"""
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row("[b]Test[/b] Application")
+        return Panel(grid, style="white")
+
+
+def update_pan(layout, image):
+    layout["main"].update(
+        Panel(f"[bold blue]{image}", border_style="green", align="right")
+    )
+    layout["command"].update(
+        Panel(
+            f"[bold blue]{image}",
+            border_style="white",
+        )
+    )
+
+
+def get_input(layout):
+    console = Console()
+    command = console.input("> ")
+    type = []
+    while True:
+        val = term.inkey()
+        command = type.append(val)
+        layout["command2"].update(Panel(f"[bold green]{command}"))
+
+
+def main():
+    images = walkimg(pics)
+    # array = len(images)
+
+    layout = make_layout()
+    layout["Title"].update(Header())
+    layout["main"].update(Panel("[bold blue]:3", border_style="green"))
+    layout["command"].update(Panel(f"[bold blue]{images[0]}", border_style="white"))
+
+    with term.cbreak(), term.hidden_cursor():
+        val = ""
+        i = 0
+        x = 1
+        y = 5
+        w = 30
+
+        with Live(layout, refresh_per_second=10, screen=True):
+            while val.lower() != "q":
+                # term.clear()
+                val = term.inkey()
+                if val.name == "KEY_DOWN" or val == "j":
+                    i -= 1
+                if val.name == "KEY_UP" or val == "k":
+                    i += 1
+                if val == "J":
+                    i -= 5
+                if val == "K":
+                    i += 5
+                if val == "n":
+                    w -= 5
+                if val == "m":
+                    w += 5
+                if val == "h":
+                    if x > 0:
+                        x -= 5
+                if val == "l":
+                    x += 5
+                if val == "f":
+                    os.system(f"feh --no-fehbg --bg-fill {images[i]}")
+                if val == "F":
+                    os.system(f"feh --bg-fill {images[i]}")
+                if val == "/":
+                    get_input(layout)
+
+                refresh(images[i], x, y, w)
+                update_pan(layout, images[i])
+
+
+main()
--- cn2.sh
+++ cn2.sh
@@ -15,9 +15,6 @@ ly=1
 array_count="${#array[@]}"
 count="(${x}/${array_count})"
 
-bg="\e[1;3;38;5;50;48;5;212m"
-clr="\e[0m"
-
 # booleans
 show_menu=false
 
@@ -25,49 +22,50 @@ show_menu=false
 tput smcup
 
 unhide_cursor() {
+    # tput cnorm
     printf '\e[?25h'
+}
+
+hide_curs() {
+    tput civis
 }
 
 cleanup() {
     unhide_cursor
     tput rmcup
+    tput cnorm
     clear
+    exit 0
 }
 
 print_menu() {
-    gum style --border=rounded --border-foreground=212 --foreground=212 \
-        --align center --width 50 --margin "1 2" --padding "2 4" \
-        "j/J - down k/K - up h - left l - right f/F - feh m/n - size + - Nyaa nyaa"
-    tput cup 1 0
+    # tput 20
+    gum style \
+        --foreground 212 --border-foreground 212 --border rounded \
+        --align left --width 50 --margin "20 30" --padding "2 4" \
+        '(j/J) to go to next' '(k/K) to go to previous' '(x) copy' \
+        '(o) open in nsxiv' '(f) set as bg' '(h/l) left/right' '(n/m) resize' '(q) to quit'
+
+    # tput cup 1 0
 
 }
 
-show_image() {
-    # tput cup 0 0
-    # tput cup $LINES 0;
-        # tput el
-        # tput cud1
+clean_image() {
+    image="${array[$i]}"
+    image="${image// /\ }"
+}
 
+show_image() {
     tput cup 0 0
     tput el
-    # printf "\e[1;35H\e[40m\e[2K%s\e[0m\e[2;0H" "${count} ${image}"
     printf "\e[40m\e[2K%s\e[0m\e[2;0H" "${count} ${image}"
     tput cup 1 0
     
     [ -f "$image" ] && { 
         kitty +kitten icat --clear --scale-up --place "${size}x${size}@${lx}x${ly}" "${image}"
-        # printf "\e[1;35H\e[40m\e[2K%s %s\e[0m\e[2;0H" "$count" "$image"
-        # tput cup $LINES 0;
-        # gum style --align=center --width=$COLUMNS \
-        #     --background=212 --foreground=50 \
-        #     --bold --italic "${count} ${image}"
-        # printf "%s%s%s" "${bg}" "$image" "$clr"
     }
-    # menx=$(($COLUMNS / 2))
-    # meny=$(($LINES / 2))
-    # tput cup "$menx" "$meny"
     if [ "$show_menu" = true ]; then
-        tput cup $((COLUMNS / 2)) 0
+        # tput cup 10 0
         print_menu
         tput cup 1 0
     fi
@@ -78,24 +76,20 @@ trap cleanup EXIT INT
 # Hide the cursor (there is probably a much better way to do this)
 printf '\e[?25l'
 
-keyhandler() {
-    case "$1" in
-    j) ;;
-    k);;
-
-    esac
-}
-
 show_image
+show_menu=true
 while true; do
+
+# menu
+    # if [ "$show_menu" = true ]; then
+    #     print_menu
+    # fi
+
     read -r -s -n 1 key < /dev/stdin > /dev/null
-    # clear below x line with tput
-    # tput cup 1 0
-    # x="$i"
+
     array_count="${#array[@]}"
     count="(${x}/${array_count})"
-    # check if count is less than 0 ie negative signed
-    # and subtract from total to get the correct index
+
     if [[ "$i" -lt 0 ]]; then
         x=$(( i + array_count ))
     fi
@@ -104,101 +98,20 @@ while true; do
         x=$(( i ))
     fi
 
-    # capture and handle keys
-    if [[ "$key" == k ]]; then
-        i=$((i+1))
-        image="${array[$i]}"
-        image="${image// /\ }"
-        show_image
-    fi
-
-    if [[ "$key" == j ]]; then
-        i=$((i-1))
-        image="${array[$i]}"
-        image="${image// /\ }"
-        show_image
-    fi
-
-    if [[ "$key" == K ]]; then
-        i=$((i+5))
-        image="${array[$i]}"
-        image="${image// /\ }"
-        show_image
-    fi
-
-    if [[ "$key" == J ]]; then
-        i=$((i-5))
-        image="${array[$i]}"
-        image="${image// /\ }"
-        show_image
-    fi
-
-    if [[ "$key" == l ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        lx=$((lx+5))
-        show_image
-    fi
-
-    if [[ "$key" == h ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        lx=$((lx-5))
-        show_image
-    fi
-
-    if [[ "$key" == n ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        size=$((size-5))
-        show_image
-    fi
-
-    if [[ "$key" == m ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        size=$((size+5))
-        show_image
-    fi
-
-    if [[ "$key" == f ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        feh --no-fehbg --bg-fill "$image"
-    fi
-
-    if [[ "$key" == F ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        feh --bg-fill "$image"
-    fi
-
-    if [[ "$key" == x ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        cb copy "$image"
-    fi
-
-    if [[ "$key" == o ]]; then
-        image="${array[$i]}"
-        image="${image// /\ }"
-        nsxiv "$image"
-    fi
-
-    if [[ "$key" == c ]]; then
-        clear
-        show_image
-    fi
-
-    if [[ "$key" == i ]]; then
-        show_menu=true
-    fi
-
-    if [[ "$key" == q ]]; then
-        break
-    fi
+case "$key" in
+j) i=$((i-1)) ; clean_image ; show_image ;;
+k) i=$((i+1)) ; clean_image ; show_image ;;
+J) i=$((i-5)) ; clean_image ; show_image ;;
+K) i=$((i+5)) ; clean_image ; show_image  ;;
+h) lx=$((lx-5)) ; clean_image ; show_image ;;
+l) lx=$((lx+5)) ; clean_image ; show_image ;;
+m) size=$((size+5)) ; clean_image ; show_image ;;
+n) size=$((size-5)) ; clean_image ; show_image ;;
+f) clean_image ; feh --no-fehbg --bg-fill "$image" ;;
+F) clean_image ; feh --bg-fill "$image" ;;
+o) clean_image ; nsxiv "$image" ;;
+c) tput clear && show_image ;;
+x) clean_image && cb copy "$image" ;;
+q) exit 0 ;;
+esac
 done
-
-image="${array[$i]}"
-image="${image// /\ }"
-printf "%s\n" "$image"
